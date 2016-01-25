4ede8328ef19bcbba9c504a8095e4f9590e0739c
catonrug <aigars.kadikis@gmail.com>
2016-01-25 18:44:08 +0200

    head

--- check.sh
+++ check.sh
@@ -6,7 +6,7 @@
 #sudo apt-get update -y && sudo apt-get upgrade -y
 #sudo apt-get install git -y
 #mkdir -p /home/pi/detect && cd /home/pi/detect
-#git clone https://github.com/catonrug/k-lite-codec-pack-detect.git && cd k-lite-codec-pack-detect && chmod +x check.sh && ./check.sh
+#git clone https://github.com/catonrug/catonrug-net-alexa-siteinfo.git && cd catonrug-net-alexa-siteinfo && chmod +x check.sh && ./check.sh
 
 #check if script is located in /home direcotry
 pwd | grep "^/home/" > /dev/null
