83132f4c45fb1e30c957bea14213821a7f4b2162
catonrug <aigars.kadikis@gmail.com>
2016-02-01 18:16:38 +0200

    constant title

--- check.sh
+++ check.sh
@@ -267,7 +267,7 @@ echo $globalrank>> $db
 emails=$(cat ../maintenance | sed '$aend of file')
 printf %s "$emails" | while IFS= read -r onemail
 do {
-python ../send-email.py "$onemail" "New alexa rank is $globalrank" "$globalrank"
+python ../send-email.py "$onemail" "New alexa rank!" "$globalrank"
 } done
 fi
 rm $tmp -rf > /dev/null
